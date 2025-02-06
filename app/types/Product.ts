ccd62534bd66d4d92bf61762f8185f67bc5c9766
Yuri Vital <fyurivs@outlook.com>
2025-02-06 13:26:56 -0300

    fix

--- app/types/Product.ts
+++ app/types/Product.ts
@@ -1,8 +1,8 @@
-type Product = {
+export type Product = {
     id: string;
     name: string;
     description: string;
     product_images: Image[];
 };
 
-type Image = { image_url: string }
+export type Image = { image_url: string }

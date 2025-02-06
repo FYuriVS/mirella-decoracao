6f50620adf5e253b251f8ffce754e8442fcfcb55
Yuri Vital <fyurivs@outlook.com>
2025-02-06 14:14:41 -0300

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
 
     return (
         <header className="sticky top-0 z-50 shadow-md bg-white">
-            <div className="container mx-auto p-1 flex items-center justify-between">
+            <div className="container mx-auto px-1 flex items-center justify-between">
                 <Link href="/" className="text-2xl font-bold p-1">
                     <Image className="rounded-full" src={"/logo.jpeg"} alt="logo" width={90} height={80} />
                 </Link>

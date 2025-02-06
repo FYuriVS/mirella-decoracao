194ab348eb70a818de5d0b6555b39a07e1b3da52
Yuri Vital <fyurivs@outlook.com>
2025-02-06 14:56:13 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lcqoazdeokbijrxfvsvg.supabase.co'],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Aplica as regras para todas as rotas
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

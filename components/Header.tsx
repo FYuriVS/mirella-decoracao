412b0af28593888121eb3be1255a9c35b0c882c6
Francisco Yuri Vital Silva <yuri@MacBook-Pro-de-Francisco.local>
2025-03-04 18:05:42 -0300

    implementa search no mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,6 @@ export function Header() {
         if (searchTerm.length > 2) {  // Só busca se houver pelo menos 3 caracteres
             const fetchResults = async () => {
                 const products = await searchProducts(searchTerm);
-
                 setResults(products);
             };
             fetchResults();
@@ -25,45 +24,80 @@ export function Header() {
     }, [searchTerm]);
 
     return (
-        <header className="sticky top-0 z-50 shadow-md bg-white">
-            <div className="container mx-auto px-1 flex items-center justify-between">
-                <Link href="/" className="text-2xl font-bold p-1">
-                    <Image className="rounded-xl" src={"/logo.jpeg"} alt="logo" width={90} height={80} />
-                </Link>
-                <div className=" hidden lg:flex items-center space-x-4">
-                    <form className="relative hidden md:flex items-center">
-                        {/* Barra de busca */}
-                        <div className="hidden lg:flex items-center bg-gray-100 rounded-full px-4 py-2">
-                            <Search className="w-5 h-5 text-gray-500" />
-                            <input
-                                type="text"
-                                placeholder="Buscar produtos..."
-                                className="bg-transparent outline-none ml-2 text-gray-800 placeholder-gray-500 w-64"
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                            />
-                        </div>
-
-                        {/* Dropdown de Resultados */}
-                        {results.length > 0 && (
-                            <div className="absolute top-full left-0 w-full bg-white shadow-md rounded-md mt-2 max-h-60 overflow-y-auto">
-                                {results.map((product: ProductType) => (
-                                    <Link
-                                        key={product.id}
-                                        href={`/details/${product.id}`}
-                                        className="block px-4 py-2 hover:bg-gray-100"
-                                        onClick={() => {
-                                            setSearchTerm("")
-                                        }}
-                                    >
-                                        {product.name}
-                                    </Link>
-                                ))}
+        <>
+            <header className="sticky top-0 z-50 shadow-md bg-white">
+                <div className="container mx-auto px-1 flex items-center justify-between">
+                    <Link href="/" className="text-2xl font-bold p-1">
+                        <Image className="rounded-xl" src={"/logo.jpeg"} alt="logo" width={90} height={80} />
+                    </Link>
+                    <div className="hidden lg:flex items-center space-x-4">
+                        <form className="relative hidden md:flex items-center">
+                            {/* Barra de busca - Visível apenas em desktop */}
+                            <div className="hidden lg:flex items-center bg-gray-100 rounded-full px-4 py-2">
+                                <Search className="w-5 h-5 text-gray-500" />
+                                <input
+                                    type="text"
+                                    placeholder="Buscar produtos..."
+                                    className="bg-transparent outline-none ml-2 text-gray-800 placeholder-gray-500 w-64"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
                             </div>
-                        )}
-                    </form>
+
+                            {/* Dropdown de Resultados */}
+                            {results.length > 0 && (
+                                <div className="absolute top-full left-0 w-full bg-white shadow-md rounded-md mt-2 max-h-60 overflow-y-auto">
+                                    {results.map((product: ProductType) => (
+                                        <Link
+                                            key={product.id}
+                                            href={`/details/${product.id}`}
+                                            className="block px-4 py-2 hover:bg-gray-100"
+                                            onClick={() => {
+                                                setSearchTerm("");
+                                            }}
+                                        >
+                                            {product.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                            )}
+                        </form>
+                    </div>
                 </div>
+            </header>
+
+            <div className="lg:hidden px-4 py-2 bg-white my-1">
+                <form className="relative flex items-center">
+                    <div className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-full">
+                        <Search className="w-5 h-5 text-gray-500" />
+                        <input
+                            type="text"
+                            placeholder="Buscar produtos..."
+                            className="bg-transparent outline-none ml-2 text-gray-800 placeholder-gray-500 w-full"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </div>
+
+                    {/* Dropdown de Resultados */}
+                    {results.length > 0 && (
+                        <div className="absolute top-full left-0 w-full bg-white shadow-md rounded-md mt-2 max-h-60 overflow-y-auto">
+                            {results.map((product: ProductType) => (
+                                <Link
+                                    key={product.id}
+                                    href={`/details/${product.id}`}
+                                    className="block px-4 py-2 hover:bg-gray-100"
+                                    onClick={() => {
+                                        setSearchTerm("");
+                                    }}
+                                >
+                                    {product.name}
+                                </Link>
+                            ))}
+                        </div>
+                    )}
+                </form>
             </div>
-        </header>
+        </>
     );
 }
